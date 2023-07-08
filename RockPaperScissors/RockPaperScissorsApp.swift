b85d689978e01ca48d78dad5201a02b0174ba644
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-07-08 17:19:34 -0500

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Muhammad Doukmak on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
